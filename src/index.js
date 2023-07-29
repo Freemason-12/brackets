1334952503075f7a8c617a52ac661a4e2d7dcfb6
Maksat <maksatr@localhost.localdomain>
2023-07-29 14:43:57 +0600

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const match = (x, y) => bracketsConfig.some((a) => a[0] === x && a[1] === y)
+  const stack = []
+  for (let i of str) {
+    if (stack.length == 0) stack.push(i)
+    else if (match(stack[stack.length - 1], i)) stack.pop()
+    else stack.push(i)
+  }
+  return stack.length === 0
 }
